c2c623587668372aaac1ab4416bf19b7be8c8694
airfortech <airm@xxx.yy>
2023-12-02 11:14:23 +0100

    added day-2-a

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -3,7 +3,35 @@ import chalk from 'chalk';
 
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const result = data
+    .map((line) => {
+      return {
+        id: +line.split(': ')[0].replace(/Game\s(\d+)/, '$1'),
+        sets: line
+          .split(': ')[1]
+          .split('; ')
+          .map((set) => {
+            const red = set.match(/(\d+) red/);
+            const green = set.match(/(\d+) green/);
+            const blue = set.match(/(\d+) blue/);
+            return {
+              red: red ? +red[1] : 0,
+              green: green ? +green[1] : 0,
+              blue: blue ? +blue[1] : 0,
+            };
+          }),
+      };
+    })
+    .filter((line) => {
+      let areSetsCorrect = true;
+      line.sets.forEach((set) => {
+        if (set.blue > 14 || set.green > 13 || set.red > 12)
+          areSetsCorrect = false;
+      });
+      return areSetsCorrect ? line : false;
+    })
+    .reduce((prev, { id }) => prev + id, 0);
+  return result;
 }
 
 const answer = await day2a();
