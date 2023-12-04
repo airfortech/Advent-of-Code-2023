7ded29c1c8bc598c1bb51101b8dcb428fbd95d98
airfortech <airm@xxx.yy>
2023-12-04 07:42:54 +0100

    added day-4

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -1,9 +1,23 @@
 import { readData } from '../../shared.ts';
+import '../../utils/Array.ts';
 import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const cards = data
+    .map((line) => line.split(': ')[1].split(' | '))
+    .map((card) =>
+      card[0]
+        .match(/.{1,3}/g)
+        .toNumbers()
+        .intersection(card[1].match(/.{1,3}/g).toNumbers())
+    )
+    .filter((card) => card.length > 0)
+    .reduce(
+      (prev, card) => prev + (card.length === 1 ? 1 : 2 ** (card.length - 1)),
+      0
+    );
+  return cards;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -1,9 +1,30 @@
 import { readData } from '../../shared.ts';
+import '../../utils/Array.ts';
 import chalk from 'chalk';
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const cards = data
+    .map((line) => line.split(': ')[1].split(' | '))
+    .map((card) =>
+      card[0]
+        .match(/.{1,3}/g)
+        .toNumbers()
+        .intersection(card[1].match(/.{1,3}/g).toNumbers())
+    )
+    .map((card) => {
+      return { wons: card.length, copies: 1 };
+    })
+    .map((card, i, arr) => {
+      if (card.wons > 0) {
+        const copies = card.copies;
+        for (let j = i + 1; j <= Math.min(card.wons + i, arr.length - 1); j++)
+          arr[j].copies += copies;
+      }
+      return card;
+    })
+    .reduce((prev, card) => prev + card.copies, 0);
+  return cards;
 }
 
 const answer = await day4b();
