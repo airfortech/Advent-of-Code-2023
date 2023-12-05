fb1ac7e565f29c43ca671a424b2e10a4ec7b0736
airfortech <airm@xxx.yy>
2023-12-05 21:15:16 +0100

    added day-5-1

--- puzzles/day-5/day-5-a.ts
+++ puzzles/day-5/day-5-a.ts
@@ -1,9 +1,47 @@
 import { readData } from '../../shared.ts';
+import '../../utils/Array.ts';
 import chalk from 'chalk';
 
 export async function day5a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let seeds = data
+    .join('\n')
+    .match(/seeds: (.*)/)[1]
+    .split(' ')
+    .toNumbers();
+  const maps: string[][] = [];
+  data.slice(2).forEach((line) => {
+    if (line.endsWith(':')) return maps.push([]);
+    if (line !== '') maps.at(-1).push(line);
+  });
+  const mapNumber = (
+    destination: number,
+    source: number,
+    count: number,
+    seed: number
+  ): number | null => {
+    return seed < source || seed > source + count - 1
+      ? null
+      : destination + seed - source;
+  };
+  const numbers = [...seeds];
+  for (let i = 0; i < numbers.length; i++) {
+    maps.forEach((sets) => {
+      sets.every((set) => {
+        const source = +set.split(' ')[1];
+        const destination = +set.split(' ')[0];
+        const count = +set.split(' ')[2];
+        const mappedNumber = mapNumber(destination, source, count, numbers[i]);
+        if (mappedNumber !== null) {
+          numbers[i] = mappedNumber;
+
+          return false;
+        }
+        return true;
+      });
+    });
+  }
+  return Math.min(...numbers);
 }
 
 const answer = await day5a();
