e57dcda1d0b4e0009f54f21e4fa2d2a75bcdfa1a
airfortech <airm@xxx.yy>
2023-12-06 19:12:23 +0100

    added day-6-2

--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -4,9 +4,14 @@ import chalk from 'chalk';
 
 export async function day6b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const time = +data[0].split(':')[1].split(/\s+/).join('');
+  const distance = +data[1].split(':')[1].split(/\s+/).join('');
+  let wins = 0;
+  for (let i = 1; i < time; i++) {
+    if ((time - i) * i > distance) wins++;
+  }
+  return wins;
 }
 
 const answer = await day6b();
-console.log(answer);
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
